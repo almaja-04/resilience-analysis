930da9b87ff016dace35b5c58b4a2609b064ab06
almaja-04 <alma.jasper@defra.gov.uk>
2025-03-11 14:48:53 +0000

    Count graph for support received and changes made.R

--- Influence_from_support_analysis.R
+++ Influence_from_support_analysis.R
@@ -13,3 +13,80 @@ use_afcharts()
 
 # Load in survey data
 data <- read.csv("FinalSurveyData.csv")
+
+support_dataframe <- data.frame(
+  q2 = c("q2_webinars", "q2_discussion_group", "q2_one_farm_visit", 
+         "q2_multiple_farm_visit", "q2_business_plan", "q2_report", 
+         "q2_carbon_audit", "q2_scheme_advice", "q2_scheme_application", 
+         "q2_1other"),
+  rename = c("Webinar", "Discussion Group", "One Farm Visit", 
+             "Multiple Farm Visits", "Develop a Business Plan", "Report", 
+             "Carbon Audit", "Scheme Advice", "Help With a Scheme Application", 
+             "Other")
+)
+
+support_pivot_props_all <- data.frame()
+
+# TABLE FOR LIKELIHOOD OF MAKING SPECIFIC CHANGES ------------------------------
+
+for (i in 1:nrow(support_dataframe)) {
+  col_name <- sym(support_dataframe[i,1])
+  
+  current_support <- data %>%
+    select(support_dataframe[i,1], starts_with("q8_")) %>%
+    filter(!!col_name == 1)
+  
+  names(current_support)[1] <- "Support Type"
+  
+  current_support <-  
+    adorn_totals(current_support, where = "row", name = support_dataframe[i,2])
+  current_support <- tail(current_support, 1)
+  rownames(current_support) <- support_dataframe[i,2]
+  current_support <- current_support[,-1]
+  colnames(current_support) <- 
+    c("Grow the business", "Reduce size of the business", 
+      "Diversify into non-farming areas", "Increase productivity", 
+      "Change core agricultural enterprises", 
+      "Leave farming (retire or pass onto next generation)", 
+      "Leave farming (other reasons)", "Other")
+  
+  support_pivot <- data.frame(as.table(as.matrix(current_support, row.names = 1)))
+  colnames(support_pivot) <- c("Support Type", "Change Made", "Freq")
+  
+  support_pivot_props <- support_pivot %>%
+    mutate(`Total per support` = ave(Freq, `Support Type`, FUN = sum)) %>%
+    mutate(Proportion = (Freq/`Total per support`))
+  
+  
+  support_pivot_props_all <- 
+    bind_rows(support_pivot_props_all, support_pivot_props)
+  
+}
+
+# GRAPHS FOR LIKELIHOOD OF MAKING SPECIFIC CHANGES -----------------------------
+
+# Count graph
+ggplot(support_pivot_props_all, aes(x = reorder(`Support Type`, `Total per support`), 
+                         y = Freq, fill = `Change Made`)) + 
+  geom_bar(stat = "identity", position = "stack") + 
+  coord_flip() + expand_limits(y = 630) + 
+  labs(
+    title = str_wrap(
+      "Types of support beneficiaries received through the Resilience Fund, and the changes they went on to make", 50), 
+    x = "Change made", y = "Count") + 
+  scale_x_discrete(labels = label_wrap(30)) + 
+  geom_text(aes(label = Freq), position = position_stack(vjust=0.5), colour = "white") + 
+  scale_fill_manual(values = 
+                      c("#C8C8C8", "#F0C571", "#59A89C", "#0B81A2", "#E25759", 
+                                 "#9D2C00", "#7E4794", "#36B700")) +
+  theme(axis.title.y = element_text(angle = 90, vjust = 2),
+        panel.grid.major.y = element_line(colour = "white"),
+        panel.grid.major.x = element_line(colour = "grey")
+        
+  )
+
+# TABLE FOR LIKELIHOOD OF INFLUENCE ON SPECIFIC CHANGES ------------------------
+
+#current_support <- data %>%
+#  select("q2_webinars", starts_with("q9_")) %>%
+#  filter(q2_webinars == 1)
