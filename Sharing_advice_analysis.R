8bccf66329709c8e89926f357e8824e8ff4b8200
almaja-04 <alma.jasper@defra.gov.uk>
2025-04-04 09:35:22 +0100

    Upload setup

---
+++ Sharing_advice_analysis.R
@@ -0,0 +1,10 @@
+library(afcharts)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+use_afcharts()
+
+# SETUP ------------------------------------------------------------------------
+
+# Load in survey data
+data <- read.csv("FinalSurveyData.csv")
